3d630842c92d533d367a887776e590e189da0899
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2016-12-28 17:43:03 -0200

    Added new constants

--- app/constants/error.js
+++ app/constants/error.js
@@ -1,4 +1,13 @@
 module.exports = {
-	NOT_FOUND_RESOURCE: 'ERROR - 404',
-	NOT_FOUND_MESSAGE: 'Resource could not be found'
+	// Not Found Resource
+	NOT_FOUND_RESOURCE: 'ERROR: NOT FOUND',
+	NOT_FOUND_MESSAGE: 'Resource could not be found',
+
+	// Unauthorized User
+	UNAUTHORIZED: 'ERROR: UNAUTHORIZED USER',
+	UNAUTHORIZED_MESSAGE: 'Check your username and/or password',
+
+	// Conflict user
+	CONFLICT: 'ERROR: CONFLICT',
+	CONFLICT_MESSAGE: 'Conflicted user'
 };
