d3fbdc643efb0ca8aea0735f40b21763422c03ea
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2016-12-28 18:45:16 -0200

    Added User model

---
+++ app/models/user.js
@@ -0,0 +1,51 @@
+class User {
+	constructor(connection) {
+		this.connection = connection();
+
+		this.authorize = this.authorize.bind(this);
+		this.insert = this.insert.bind(this);
+		this.findUser = this.findUser.bind(this);
+	}
+
+	findUser(user, successCallback, errorCallback) {
+		this.connection.open(function (err, mongoclient) {
+			mongoclient.collection("users", function (err, collection) {
+				collection.find(user).toArray(function (err, result) {
+					let data = result[0];
+
+					if (data && typeof data === "object") {
+						successCallback(data);
+						return;
+					}
+					errorCallback();
+				});
+
+				mongoclient.close();
+			});
+		});
+	}
+
+	insert(user, callback) {
+		this.connection.open(function (err, mongoclient) {
+			mongoclient.collection("users", function (err, collection) {
+				collection.insert(user);
+				callback(user);
+
+				mongoclient.close();
+			});
+		});
+	}
+
+	authorize(user, successCallback, errorCallback) {
+		this.findUser(
+			user,
+			(data) => successCallback(data),
+			errorCallback
+		);
+	}
+}
+
+module.exports = function (app) {
+	return new User(app.configs.dbConnection);
+};
+
