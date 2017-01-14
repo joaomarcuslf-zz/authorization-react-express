c3d3135eb9e85938a3fbea0fa85b3b2bcf57ffae
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2017-01-14 12:42:52 -0200

    Remove the use of Arrow Function from Mocha tests

--- authorization-express-api/specs/app/helpers/cryptoHelper.spec.js
+++ authorization-express-api/specs/app/helpers/cryptoHelper.spec.js
@@ -3,29 +3,29 @@ const expect = require('../../test-helper').expect;
 const CryptoHelper = require('../../../app/helpers/cryptoHelper');
 const mockedString = 'mockedString';
 
-describe('CryptoHelper', () => {
-	describe('crypt', () => {
-		it('should give an md5 string if no format is given', () => {
+describe('CryptoHelper', function() {
+	describe('crypt', function() {
+		it('should give an md5 string if no format is given', function() {
 			let cryptResult = CryptoHelper.crypt(mockedString);
 
 			expect(cryptResult.length).to.be.equal(32);
 		});
 
-		it('should generate an random string if nothing is given', () => {
+		it('should generate an random string if nothing is given', function() {
 			let cryptResult = CryptoHelper.crypt();
 
 			expect(cryptResult.length).to.be.equal(32);
 		});
 
-		it('should give an sha256 string if this format is given', () => {
+		it('should give an sha256 string if this format is given', function() {
 			let cryptResult = CryptoHelper.crypt(mockedString, 'sha256');
 
 			expect(cryptResult.length).to.be.equal(64);
 		});
 	});
 
-	describe('generatePasswordHash', () => {
-			it('should generate and sha256 string', () => {
+	describe('generatePasswordHash', function() {
+			it('should generate and sha256 string', function() {
 				let cryptResult = CryptoHelper.generatePasswordHash(mockedString);
 
 				expect(cryptResult.length).to.be.equal(64);
--- authorization-express-api/specs/app/helpers/tokenHelper.spec.js
+++ authorization-express-api/specs/app/helpers/tokenHelper.spec.js
@@ -3,17 +3,17 @@ const expect = require('../../test-helper').expect;
 const TokenHelper = require('../../../app/helpers/tokenHelper');
 const mockedString = 'mockedString';
 
-describe('TokenHelper', () => {
-	describe('generateToken', () => {
-		it('should return and encoded token', () => {
+describe('TokenHelper', function() {
+	describe('generateToken', function() {
+		it('should return and encoded token', function() {
 			let result = TokenHelper.generateToken(mockedString);
 
 			expect(result.length).to.be.equal(100);
 		});
 	});
 
-	describe('getTokenData', () => {
-		it('should be able to decrypt the token', () => {
+	describe('getTokenData', function() {
+		it('should be able to decrypt the token', function() {
 			let token = TokenHelper.generateToken(mockedString);
 			let result = TokenHelper.getTokenData(token);
 
