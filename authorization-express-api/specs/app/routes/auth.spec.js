a29db995b0595220fec2da3a0a6b34cc070c25fe
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2017-01-15 12:53:36 -0200

    Added tests for signup rout

--- authorization-express-api/specs/app/routes/auth.spec.js
+++ authorization-express-api/specs/app/routes/auth.spec.js
@@ -101,7 +101,7 @@ describe('auth routes', function () {
 					});
 			});
 
-			it('should send status 401 unauthorized user', function(done) {
+			it('should send status 401 UNAUTHORIZED user', function(done) {
 				request(app)
 					.post('/api/v1/login')
 					.send({ "username": "mock0", "password": "mock0" })
@@ -162,6 +162,167 @@ describe('auth routes', function () {
 		});
 	});
 
-	describe('POST /api/v1/signup', function() {
+	describe('POST /api/v1/signup', function () {
+		describe('with invalid body request', function () {
+			it('should have content type json', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.expect('Content-Type', /json/)
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+
+			it('should send status 400 for no body', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.expect('Content-Type', /json/)
+					.expect(400)
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+
+			it('should send and body describing the empty field(test 1)', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.expect([
+						{ "field": "username", "message": "Username can't be empty" },
+						{ "field": "password", "message": "Password can't be empty" },
+						{ field: 'email', message: 'E-mail can\'t be empty' }
+					])
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+
+			it('should send and body describing the empty field(test 2)', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.send({ "username": "mock0" })
+					.expect([
+						{ "field": "password", "message": "Password can't be empty" },
+						{ field: 'email', message: 'E-mail can\'t be empty' }
+					])
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+
+			it('should send and body describing the empty field(test 3)', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.send({ "password": "mock0" })
+					.expect([
+						{ "field": "username", "message": "Username can't be empty" },
+						{ field: 'email', message: 'E-mail can\'t be empty' }
+					])
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+
+			it('should send and body describing the empty field(test 4)', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.send({ "email": "mock0" })
+					.expect([
+						{ "field": "username", "message": "Username can't be empty" },
+						{ "field": "password", "message": "Password can't be empty" }
+					])
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+		});
+
+		describe('valid body but conflict user', function () {
+			beforeEach(function (done) {
+				connection().open(function (err, mongoclient) {
+					mongoclient.collection("users", function (err, collection) {
+						collection.insert(userMocked);
+
+						mongoclient.close();
+						done();
+					});
+				});
+			});
+
+			afterEach(function (done) {
+				connection().open(function (err, mongoclient) {
+					mongoclient.collection("users", function (err, collection) {
+						collection.remove({});
+
+						mongoclient.close();
+						done();
+					});
+				});
+			});
+
+			it('should have content type json', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.send({ username: 'mock0', password: 'mock0', email: 'mock@mock.mock' })
+					.expect('Content-Type', /json/)
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+
+			it('should send status 409 for CONFLICT user', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.send({ username: 'mock0', password: 'mock0', email: 'mock@mock.mock' })
+					.expect('Content-Type', /json/)
+					.expect(409)
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+		});
+
+		describe('valid body and new user', function () {
+			beforeEach(function (done) {
+				connection().open(function (err, mongoclient) {
+					mongoclient.collection("users", function (err, collection) {
+						collection.remove({});
+
+						mongoclient.close();
+						done();
+					});
+				});
+			});
+
+			it('should have content type json', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.send({ username: 'mock0', password: 'mock0', email: 'mock@mock.mock' })
+					.expect('Content-Type', /json/)
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+
+			it('should send status 200 for SUCCESS user', function(done) {
+				request(app)
+					.post('/api/v1/signup')
+					.send({ username: 'mock0', password: 'mock0', email: 'mock@mock.mock' })
+					.expect('Content-Type', /json/)
+					.expect(200)
+					.end(function(err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
+		});
 	});
 });
