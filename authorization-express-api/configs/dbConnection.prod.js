0818af15be3c78a0ad67902e9f6f74e14a0cfd74
João Marcus Fernandes <jmarcusfernandes@gmail.com>
2017-01-25 23:19:14 -0200

    Removed the prod connection

--- authorization-express-api/configs/dbConnection.prod.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const mongoose = require('mongoose');
-
-const connMongoDB = function(){
-	return mongoose.createConnection('mongodb://admin:admin@ds133358.mlab.com:33358/authorization-react-express');
-}
-
-module.exports = connMongoDB;
